91b10fef221d037f73f193bf2d2efa81296c21e0
wolz-CODElife <joeladewole3@gmail.com>
2021-01-04 17:49:44 +0100

    Update list_comparison.py

--- list_comparison.py
+++ list_comparison.py
@@ -1,6 +1,13 @@
 '''
-In this file we have examples of list comparisons
+In this file we have examples of list/array comparisons
 '''
 
-#here is an example of normal list manipulation
+#here is an example of normal list/array manipulation
 list = []
+print(list)
+
+#here is an example of adding a value to an list/array
+list.append(2)
+print(list)
+
+
