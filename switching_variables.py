66b0448b86288ce6cdae808dcd7a510776c71053
wolz-CODElife <joeladewole3@gmail.com>
2021-01-12 21:55:37 +0100

    Update switching_variables.py

--- switching_variables.py
+++ switching_variables.py
@@ -1,1 +1,7 @@
+#this file contains example on how 
+#to switch two variable between themselves
+#without introducing new variables
 
+def main():
+    first = input('Please input a word or number')
+    swap(first, last)
