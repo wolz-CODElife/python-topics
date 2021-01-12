367f315befcc1282886970e14de5acf008216f1d
wolz-CODElife <joeladewole3@gmail.com>
2021-01-12 22:08:30 +0100

    Update switching_variables.py

--- switching_variables.py
+++ switching_variables.py
@@ -2,11 +2,15 @@
 #to switch two variable between themselves
 #without introducing new variables
 
+#define the main program which will make use of 
+#an external function
 def main():
     first = input('Please input a word or number')
     last = input('Please input a word or number')
     swap(first, last)
 
+#define an external function(OPTIONAL) that will 
+#switch the variable
 def swap(first, last):
     first, last = last, first
     print('First: ', first, '/', 'Last: ', last)
